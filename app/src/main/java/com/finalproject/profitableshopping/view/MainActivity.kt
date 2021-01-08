ec75ebfe52ff9d8105ba71f55f9da45a898b8118
abdullahalamodi <abdullahalamodi@bootfi.com>
2021-01-08 22:06:10 +0300

    init project structure

--- app/src/main/java/com/finalproject/profitableshopping/view/MainActivity.kt
+++ app/src/main/java/com/finalproject/profitableshopping/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.finalproject.profitableshopping
+package com.finalproject.profitableshopping.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.finalproject.profitableshopping.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
